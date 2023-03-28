f789acd33914abb69b04311b1f786b026848716d
MattBanuag <matthewbanuag.dev@gmail.com>
2023-03-28 12:12:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using AdventureWorksAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb")));
+
+var app = builder.Build();
+
+/*----- END POINTS ----- */
+
+// Address
+
+// Customer
+
+// Product
+
+// SalesOrderHeader
