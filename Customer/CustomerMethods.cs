b84169c6dc1ca4a9f0ddee1b01e8f9aedb928c6f
MattBanuag <matthewbanuag.dev@gmail.com>
2023-03-28 13:27:56 -0500

    create: customerMethods class and getCustomers method

---
+++ Customer/CustomerMethods.cs
@@ -0,0 +1,12 @@
+﻿using AdventureWorksAPI.Models;
+
+namespace AdventureWorksAPI.Customer
+{
+    public static class CustomerMethods
+    {
+        public static IResult GetCustomers(AdventureWorksLt2019Context db, int maxResults = 100)
+        {
+            return Results.Ok(db.Products.Take(maxResults).ToList());
+        }
+    }
+}
