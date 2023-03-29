a836e813dd33bc7473191d6f49a38a76d1d500a6
Gurveer <gurveer2202@gmail.com>
2023-03-29 15:45:09 -0500

    Completed methods for getting customers in addresses

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using AdventureWorksAPI.SaleOrderHeader;
 using Lab3.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,8 @@ builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksDb"));
 });
+builder.Services.AddControllersWithViews()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
 /*----- END POINTS ----- */
@@ -23,6 +26,9 @@ app.MapGet("/address", MethodForAddress.GetAddress);
 app.MapPost("/address", MethodForAddress.Create);
 app.MapDelete("/address/delete", MethodForAddress.Delete);
 app.MapPut("/address/update", MethodForAddress.Update);
+
+//Methods
+app.MapGet("/address/details", MethodForAddress.FindCustomerInAddress);
 
 // Customer
 app.MapGet("/customers/{id?}", CustomerMethods.GetCustomers);
