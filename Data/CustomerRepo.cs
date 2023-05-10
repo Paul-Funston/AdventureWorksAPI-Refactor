60c31d186d74bee9b300cfd2b55150308dc2ce7c
Reagan615 <419247451@qq.com>
2023-05-10 10:39:55 -0500

    again

--- Data/CustomerRepo.cs
+++ Data/CustomerRepo.cs
@@ -1,6 +1,40 @@
-﻿namespace AdventureWorksAPI.Data
+﻿using AdventureWorksAPI.Models;
+
+namespace AdventureWorksAPI.Data
 {
-    public class CustomerRepo
+    public class CustomerRepo : ICustomerRepo
     {
+        
+        private AdventureWorksLt2019Context _context;
+        public CustomerRepo(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+        public ICollection<Customer> GetCustomers() 
+        {
+            return _context.Customers.ToList();
+        }
+        public Customer GetCustomer(int id)
+        {
+            return _context.Customers.Find(id);
+        }
+
+        public void CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            Customer updatingCustomer = _context.Customers.Find(id);
+
+            if (updatingCustomer == null)
+            {
+                _context.Customers.Add(customer);
+
+                _context.SaveChanges();
+            } 
+        }
+        
     }
 }
